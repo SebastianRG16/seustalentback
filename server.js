6daa19f5fb35bcfe1a6369127db162cbda8a8ad3
SebastianRG16 <89326030+SebastianRG16@users.noreply.github.com>
2024-12-26 08:08:04 -0500

    PORT deploy

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/generalinformation", generalinformation);
 app.use("/academicformation", academicformation);
 app.use("/workexperience", workexperience);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
